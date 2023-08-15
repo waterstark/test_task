f1c652c88c7e70146612fbe9069ad547f09c43fa
waterstark <84220220+waterstark@users.noreply.github.com>
2023-08-15 16:27:40 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+from pprint import pprint
 import requests
 import csv
 from openpyxl import Workbook
 
 from config import SECURITY_KEY
+
+
+def access_token(func):
+    def wrapper(*args, **kwargs):
+        responce = func(*args, **kwargs)
+        if responce == 401:
+            pyrus.get_access_token()
+            res = func(*args, **kwargs)
+            return res
+        return responce
+
+    return wrapper
 
 
 class PyrusClient:
@@ -17,36 +30,38 @@ class PyrusClient:
         )
         self.access_token = f"Bearer {r.json()['access_token']}"
 
+    @access_token
     def get_id_from_form(self):
         headers = {"Authorization": self.access_token}
         url = "https://api.pyrus.com/v4/forms"
         r = requests.get(url, headers=headers)
+        if r.status_code == 401:
+            return r.status_code
         forms = r.json()
-        if r.status_code != 200:
-            self.get_access_token()
-            headers = {"Authorization": self.access_token}
-            r = requests.get(url, headers=headers)
-            if r.status_code != 200:
-                return None
-            return r.json()
         return forms["forms"][0]["id"]
 
+    @access_token
     def get_tasks(self, form_id):
-        url = f"https://api.pyrus.com/v4/forms/{form_id}/register"
         headers = {"Authorization": self.access_token}
-
+        url = f"https://api.pyrus.com/v4/forms/{form_id}/register"
         r = requests.get(url, headers=headers)
+        if r.status_code == 401:
+            return r.status_code
+        tasks = r.json()
+        return tasks
 
-        return r.json()
-
+    @access_token
     def download_file_from_form(self, file_id):
         headers = {"Authorization": self.access_token}
         url = f"https://api.pyrus.com/v4/files/download/{file_id}"
         r = requests.get(url, stream=True, headers=headers)
+        if r.status_code == 401:
+            return r.status_code
         with open(f"test.txt", "wb") as file:
             for chunk in r.iter_content(chunk_size=128):
                 file.write(chunk)
 
+    @access_token
     def upload_file(self, file_path):
         url = "https://api.pyrus.com/v4/files/upload"
         headers = {
@@ -56,14 +71,12 @@ class PyrusClient:
 
         r = requests.post(url, headers=headers, files=files)
 
-        if r.status_code != 200:
-            self.get_access_token()
-            headers = {
-                "Authorization": self.access_token,
-            }
+        if r.status_code == 401:
+            return r.status_code
 
         return r.json()
 
+    @access_token
     def add_attachment(self, task_id, file_id):
         headers = {"Authorization": self.access_token}
         url = f"https://api.pyrus.com/v4/tasks/{task_id}/comments"
@@ -75,8 +88,11 @@ class PyrusClient:
             },
             headers=headers,
         )
+        if r.status_code == 401:
+            return r.status_code
         return r.json()
 
+    @access_token
     def change_task_field(self, task_id, guid):
         headers = {"Authorization": self.access_token}
         url = f"https://api.pyrus.com/v4/tasks/{task_id}/comments"
@@ -92,12 +108,17 @@ class PyrusClient:
             },
             headers=headers,
         )
+        if r.status_code == 401:
+            return r.status_code
         return r.json()
 
+    @access_token
     def download_tasks_from_form(self, form_id):
         headers = {"Authorization": self.access_token}
         url = f"https://api.pyrus.com/v4/forms/{form_id}/register?format=csv"
         r = requests.get(url, headers=headers, stream=True)
+        if r.status_code == 401:
+            return r.status_code
 
         decoded_content = r.content.decode("utf-8")
         reader = csv.reader(decoded_content.splitlines(), delimiter=",")
@@ -112,10 +133,9 @@ class PyrusClient:
 
 pyrus = PyrusClient()
 
-pyrus.get_access_token()
+# pyrus.get_access_token()
 form_id = pyrus.get_id_from_form()
 tasks = pyrus.get_tasks(form_id)
-
 # Выбран конкретный id для созданной мной таски
 task_id = tasks["tasks"][0]["id"]
 file_id = tasks["tasks"][0]["fields"][11]["value"]["fields"][5]["value"][0]["id"]
